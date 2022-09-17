ebcb7ba8938f92113bf78de6020abf327c554b81
Dilipan M <dilipan12m@gmail.com>
2022-09-17 22:41:31 +0530

    ui to display profile added

--- app.js
+++ app.js
@@ -1,17 +1,20 @@
 const github = new Github();
 
+const ui = new UI();
+
 const userId = document.getElementById("searchUser");
 
 userId.addEventListener("keyup", (e) => {
-  if (e.target.value !== "") {
-    github.getProfile(e.target.value).then((data) => {
-      if (data.profileData.message === "Not Found") {
-        // no user found
-      } else {
-        // display ui profile
-      }
-    });
-  } else {
-    //clear prfile
-  }
+    if (e.target.value !== "") {
+        github.getProfile(e.target.value).then((data) => {
+            if (data.profileData.message === "Not Found") {
+                // no user found
+            } else {
+                // display ui profile
+                ui.renderProfile(data.profileData);
+            }
+        });
+    } else {
+        //clear prfile
+    }
 });
